3d6181750624683e797e153c0cd867a8be288639
SpiralMoon <mjh1660@gmail.com>
2021-08-04 18:37:32 +0900

    Update tsconfig

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index";
+import apiRouter from "./routes/api";
 
 import {Request, Response, NextFunction} from "express";
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', indexRouter);
+app.use('/', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req:Request, res:Response, next:NextFunction) {
